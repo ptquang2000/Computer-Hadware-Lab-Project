7da4fe8f648d5216add04efa445a3d9bcfb8467e
ptquang <ptquang2000@gmail.com>
2021-04-03 22:04:59 +0700

    Setting new data structure

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -3,23 +3,23 @@ const configLink = document.querySelector('.add-config');
 const configForm = document.querySelector('.new-config form');
 
 configLink.addEventListener('click', ()=>{
-    configModal.classList.add('open')
+	configModal.classList.add('open')
 })
 
 configModal.addEventListener('click', (e)=>{
-    if (e.target.classList.contains('new-config')){
-        configModal.classList.remove('open')
-    }
+	if (e.target.classList.contains('new-config')){
+			configModal.classList.remove('open')
+	}
 })
 
 configForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    
-    const username = firebase.auth().currentUser.email.split('@')[0];
-    var updates = {};
-    updates['/users/' + username + '/' + configForm.date.value + '/' + configForm.timeset.value] = configForm.duration.value;    
-    firebase.database().ref().update(updates);
-    configForm.reset();
-    configModal.classList.remove('open');
-    configForm.querySelector('.error').textContent = '';    
+	e.preventDefault();
+	
+	const username = firebase.auth().currentUser.email.split('@')[0];
+	var updates = {};
+	updates['/users/' + username + '/' + configForm.date.value + '/' + configForm.timeset.value] = configForm.duration.value;    
+	firebase.database().ref().update(updates);
+	configForm.reset();
+	configModal.classList.remove('open');
+	configForm.querySelector('.error').textContent = '';    
 })
--- web/public/js/config.js
+++ web/public/js/config.js
@@ -1,28 +1,28 @@
 const setupList = (datas) => {
-    let days = [];
-    if (datas){
-        Object.entries(datas).map(data => {
-            if (!data[0].includes('id') && !data[0].includes('username'))
-                days.push({...data[1], date: data[0]});
-          })
-        let html = ``;
-        days.forEach(day =>{
-            Object.entries(day).map(config => {
-                if (!config[0].includes('date')){
-                    html += `<li config="${day.date}${config[0]}">
-                          <span class="text">${day.date} ${config[0]} </span>
-                          <div>
-                          <span class="text">${config[1]}</span>
-                          <button type="button" onclick="delFunc('${day.date}${config[0]}','${datas.username}')">delete</buton>
-                          </div>
-                          </li>`;
-                }
-            })
-        })
-        document.querySelector('ul').innerHTML = html;
-    }
+	let days = [];
+	if (datas){
+		Object.entries(datas).map(data => {
+			if (!data[0].includes('id') && !data[0].includes('username'))
+					days.push({...data[1], date: data[0]});
+		})
+		let html = ``;
+		days.forEach(day =>{
+			Object.entries(day).map(config => {
+				if (!config[0].includes('date')){
+					html += `<li config="${day.date}${config[0]}">
+								<span class="text">${day.date} ${config[0]} </span>
+								<div>
+								<span class="text">${config[1]}</span>
+								<button type="button" onclick="delFunc('${day.date}${config[0]}','${datas.username}')">delete</buton>
+								</div>
+								</li>`;
+				}
+			})
+		})
+		document.querySelector('ul').innerHTML = html;
+	}
 };
 
 function delFunc(datetime, username) {
-  firebase.database().ref('users/' + username + '/' + datetime.slice(0, -9) + '/' + datetime.slice(-9)).remove();
+	firebase.database().ref('users/' + username + '/' + datetime.slice(0, -9) + '/' + datetime.slice(-9)).remove();
 }
