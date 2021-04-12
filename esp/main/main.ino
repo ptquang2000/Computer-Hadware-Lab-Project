c07b4116f600877648c0315dc4f1da94dd855fcc
ptquang <ptquang2000@gmail.com>
2021-04-12 22:23:03 +0700

    able to ask for fb email/pass

--- esp/main/main.ino
+++ esp/main/main.ino
@@ -1,18 +1,22 @@
+#include <FS.h>
+#include <ArduinoJson.h> 
+
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
 
 #define FIREBASE_HOST "watering-plant-system-default-rtdb.firebaseio.com"
 #define API_KEY "AIzaSyB_zu-rVb6E1lGinalWvVw9pAZ6aSMa9DQ"
 
-#define USER_EMAIL "user_abc_123@gmail.com"
-#define USER_PASSWORD "123abcuser"
+// #define USER_EMAIL "user_abc_123@gmail.com"
+// #define USER_PASSWORD "123abcuser"
 
 FirebaseData fbdo;
 
 FirebaseAuth auth;
 FirebaseConfig config;
 
-String path = "users/user_abc_123";
+// String path = "users/user_abc_123";
+String path = "users/";
 
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
@@ -27,6 +31,17 @@ NTPtime NTPch("ch.pool.ntp.org");
 
 #define RELAY 4
 
+//flag for saving data
+bool shouldSaveConfig = false;
+char user_email[40];
+char user_password[40];
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
 void tick()
 {
   int state = digitalRead(BUILTIN_LED);  
@@ -40,6 +55,23 @@ void configModeCallback (WiFiManager *myWiFiManager) {
   ticker.attach(0.2, tick);
 }
 
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
 void setup() {
   pinMode(RELAY, OUTPUT);
 
@@ -50,7 +82,54 @@ void setup() {
 
   ticker.attach(0.6, tick);
 
+	// clean FS, for testing
+  SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+          strcpy(user_email, json["user_email"]);
+					strcpy(user_password, json["user_password"]);
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+  
+  WiFiManagerParameter custom_user_email("user_email", "user_email", user_email, 40);
+  WiFiManagerParameter custom_user_password("user_password", "user_password", user_password, 40);
+
   WiFiManager wifiManager;
+	// wifiManager.resetSettings();
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+	wifiManager.addParameter(&custom_user_email);
+	wifiManager.addParameter(&custom_user_password);
+	wifiManager.setCustomHeadElement("<script>document.getElementById('user_password').setAttribute('type','password')</script>");
 
   wifiManager.setAPCallback(configModeCallback);
 
@@ -64,14 +143,38 @@ void setup() {
   ticker.detach();
   digitalWrite(BUILTIN_LED, LOW);
 
+	strcpy(user_email, custom_user_email.getValue());
+	strcpy(user_password, custom_user_password.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["user_email"] = user_email;
+    json["user_password"] = user_password;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
 	config.host = FIREBASE_HOST;
   config.api_key = API_KEY;
 
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
+  auth.user.email = user_email;
+  auth.user.password = user_password;
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+
+	path = path + getValue(user_email,'@',0);
 
 }
 
